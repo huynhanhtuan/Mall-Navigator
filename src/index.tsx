b4c708977fdba76db20a856f77333b3eaf607f76
tuan.huynh <tuanha@hisoft.vn>
2025-03-27 14:26:35 +0700

    initial

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from 'react-router-dom';
+// import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Router basename="/Mall-Navigator">
-      <App />
-    </Router>
+    <App />
   </React.StrictMode>
 );
 
